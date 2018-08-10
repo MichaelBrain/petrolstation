48ec66ca6728251abf05268745b29845cbad009f
Ryan Corcoran <ryan_corcoran@live.co.uk>
2018-08-10 15:24:34 +0100

    dynamic pump generation added and car spawning probability

--- petrolstation/src/com/company/Main.java
+++ petrolstation/src/com/company/Main.java
@@ -16,20 +16,18 @@ public class Main {
 
     public static void setUp(){
         PetrolStation station = new PetrolStation();
-        Pump pump1 = new Pump();
-        Pump pump2 = new Pump();
-        Pump pump3 = new Pump();
-        Pump pump4 = new Pump();
+
+        List<Pump> pumpList = new ArrayList<Pump>();
+        for (int i=0; i < station.noOfPumps; i++)
+        {
+        	Pump p = new Pump();
+        	pumpList.add(p);
+        }
+        station.pumps = pumpList;
 
         System.out.println(SPACER);
         System.out.println("Petrol Station is open for business");
         System.out.println(SPACER);
-
-        station.pumps = new ArrayList<Pump>();
-        station.pumps.add(pump1);
-        station.pumps.add(pump2);
-        station.pumps.add(pump3);
-        station.pumps.add(pump4);
 
         System.out.println("There are currently " + station.pumps.size() + " pumps operating");
         System.out.println(SPACER);
